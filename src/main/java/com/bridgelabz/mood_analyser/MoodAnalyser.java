5147424e2dfb555989fe1d4d1d00025f98c42750
tushar.kasturi_ymedi <tusharkasturi1999@gmail.com>
2021-09-08 20:10:13 +0530

    Added: analyse mood method and test cases

--- src/main/java/com/bridgelabz/mood_analyser/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/mood_analyser/MoodAnalyser.java
@@ -1,7 +1,12 @@
 package com.bridgelabz.mood_analyser;
 
+/**
+ * @author tushar.kasturi_ymedi
+ *
+ */
 public class MoodAnalyser {
 
+	//This method gives SAD or HAPPY mood
 	public String analyseMood(String message) {
 		if (message.contains("sad")) {
 			return "SAD";
--- src/test/java/com/bridgelabz/mood_analyser/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/mood_analyser/MoodAnalyserTest.java
@@ -5,7 +5,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for MoodAnalyser.
  */
 public class MoodAnalyserTest 
 {
